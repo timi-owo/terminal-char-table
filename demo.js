5870dc38e59c66f74481fa16e9d609a689d5f34a
timi-owo <781662377@qq.com>
2020-09-14 11:55:39 +0800

    Code improvement and new features

---
+++ demo.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const CharTable = require('./module');
+
+function demo1()
+{
+	console.log('\n> Demo1: Create a simple table with default options.');
+
+	let table = new CharTable();
+
+	// fill the table row by rows
+	table.append(['1', 'xxx', 'xxx', ':)', '?']);
+	table.append(['2', 'xxx', 'xxx']);
+	table.append(['3', 'xxx', 'xxx', ':3']);
+	table.append(['10', 'xxx', 'xxx', '', 'owo']);
+
+	// this row will be the table header
+	table.insert(['#', 'column1', 'column2']);
+
+	// output table with 1 indentation
+	console.log(table.string(1));
+}
+
+function demo2()
+{
+	console.log('\n> Demo2: Only draw columns if it\'s not empty.');
+
+	let table = new CharTable({ column_empty_drawn: false });
+
+	// table header
+	table.append(['#', 'column1', 'column2']);
+
+	// table body
+	table.from([['1', 'xxx', 'xxx', ':)', '?'], ['2', 'xxx', 'xxx'], ['3', 'xxx', 'xxx', ':3']]);
+
+	// output table with 1 indentation
+	console.log(table.string(1));
+}
+
+function demo3()
+{
+	console.log('\n> Demo3: Apply a different alignment to each rows.');
+
+	// make columns to center alignment by default and looks more wider
+	let table = new CharTable({ column_align: 'center', column_fill_length: 4 });
+
+	// table header (left alignment)
+	table.append(['#', 'data', 'alignment'], 'left');
+
+	// 1~3 rows (left alignment)
+	table.from([['1', '-----', 'left'], ['2', '-----', 'left'], ['3', '-----', 'left']], 'left');
+
+	// 4~6 rows (center alignment by default)
+	table.from([['4', '-----', 'center'], ['5', '-----', 'center'], ['6', '-----', 'center']]);
+
+	// 7~9 rows (right alignment)
+	table.from([['7', '-----', 'right'], ['8', '-----', 'right'], ['9', '-----', 'right']], 'right');
+
+	// another way to make this work (Uncomment if you want to try)
+	/*
+	table.from2([
+		{
+			columns: ['111', '-----', 'left'],
+			align: 'left'
+		},
+		{
+			columns: ['222', '-----', 'center'],
+		},
+		{
+			columns: ['333', '-----', 'right'],
+			align: 'right'
+		}
+	]);
+	*/
+
+	// output table with 1 indentation
+	console.log(table.string(1));
+}
+
+demo1();
+demo2();
+demo3();
+
+//...
--- module.js
+++ module.js
@@ -1,5 +1,5 @@
-// Original Author: https://github.com/hustcc/word-table
 // Github: https://github.com/timi-owo/terminal-char-table
+// NPM JS: https://www.npmjs.com/package/terminal-char-table
 
 'use strict';
 
@@ -7,11 +7,21 @@
 
 class CharTable
 {
-	constructor(column_fill_width = 3)
+	constructor(option = undefined)
 	{
-		this.m_rowData = [];
-		this.m_colMaxLen = [];
-		this.m_colFillWidth = column_fill_width;
+		this.m_metaRows = [];
+		this.m_colBaseLen = [];
+
+		this.m_tableOpt =
+		{
+			...
+			{
+				column_align: 'left',
+				column_fill_length: 2,
+				column_empty_drawn: true
+			},
+			...option
+		};
 
 		this.m_cachedTableLines = [];
 		this.m_markUpdated = false;
@@ -20,92 +30,14 @@ class CharTable
 	_fillStr(char, len)
 	{
 		let str = '';
-		for (let i = 0; i < len; i++) { str += char; }
+		while (str.length < len) { str += char; }
 		return str;
-	}
-
-	_drawDiv()
-	{
-		let str = '+';
-		let len = this.m_colMaxLen.length;
-
-		for (let i = 0; i < len; i++)
-		{
-			let width = this.m_colMaxLen[i] + this.m_colFillWidth * 2;
-			str += this._fillStr('-', width);
-			str += '+';
-		}
-		return str;
-	}
-
-	_drawRow(row)
-	{
-		let str = '|';
-		let len = this.m_colMaxLen.length;
-
-		let columns = this.m_rowData[row];
-		let columns_len = columns.length;
-
-		for (let i = 0; i < len; i++)
-		{
-			if (i < columns_len)
-			{
-				let width = (this.m_colMaxLen[i] - columns[i].colTextLen) / 2;
-				str += this._fillStr(' ', Math.floor(width) + this.m_colFillWidth);
-				str += columns[i].colText;
-				str += this._fillStr(' ', Math.ceil(width) + this.m_colFillWidth);
-				str += '|';
-			}
-			else
-			{
-				str += this._fillStr(' ', this.m_colMaxLen[i] + this.m_colFillWidth * 2);
-				str += '|';
-			}
-		}
-		return str;
-	}
-
-	_drawTable()
-	{
-		this.m_cachedTableLines = [];
-		let len = this.m_rowData.length;
-
-		for (let row = 0; row < len; row++)
-		{
-			if (row == 0) { this.m_cachedTableLines.push(this._drawDiv()); }
-			this.m_cachedTableLines.push(this._drawRow(row));
-			this.m_cachedTableLines.push(this._drawDiv());
-		}
-	}
-
-	_buildRowColumns(fields)
-	{
-		let columns = [];
-		let length = fields.length;
-
-		for (let i = 0; i < length; i++)
-		{
-			let charlen = this._measureStrLen(fields[i]);
-
-			if (this.m_colMaxLen.length > i)
-			{
-				this.m_colMaxLen[i] = Math.max(charlen, this.m_colMaxLen[i]);
-			}
-			else { this.m_colMaxLen.push(charlen); }
-
-			let obj =
-			{
-				colText: fields[i],
-				colTextLen: charlen
-			};
-			columns.push(obj);
-		}
-		return columns;
 	}
 
 	_measureStrLen(str)
 	{
 		let len = 0;
+
 		for (let i = 0; i < str.length; i++)
 		{
 			let code = str.charCodeAt(i);
@@ -115,8 +47,130 @@ class CharTable
 		return len;
 	}
 
+	_buildRowData(fields, align)
+	{
+		if (align == undefined || (align != 'left' && align != 'center' && align != 'right'))
+		{
+			// prevent illegal input
+			align = this.m_tableOpt.column_align;
+		}
+
+		let row_data =
+		{
+			colFields: [],
+			colAlign: align
+		};
+
+		for (let i = 0; i < fields.length; i++)
+		{
+			let charlen = this._measureStrLen(fields[i]);
+
+			if (i < this.m_colBaseLen.length)
+			{
+				// existed column, update length if current column is bigger.
+				this.m_colBaseLen[i] = Math.max(charlen, this.m_colBaseLen[i]);
+			}
+			else { this.m_colBaseLen.push(charlen); } // new column
+
+			let column =
+			{
+				colText: fields[i],
+				colTextLen: charlen
+			};
+			row_data.colFields.push(column);
+		}
+		return row_data;
+	}
+
+	_drawTable()
+	{
+		this.m_cachedTableLines = [];
+
+		for (let row = 0; row < this.m_metaRows.length; row++)
+		{
+			if (row == 0) { this.m_cachedTableLines.push(this._drawDiv(row, true)); }
+			this.m_cachedTableLines.push(this._drawRow(row));
+			this.m_cachedTableLines.push(this._drawDiv(row));
+		}
+	}
+
+	_drawDiv(row, first_line = false)
+	{
+		let str = '+';
+		let columns = this.m_metaRows[row].colFields;
+
+		for (let col = 0; col < this.m_colBaseLen.length; col++)
+		{
+			if (col < columns.length || this.m_tableOpt.column_empty_drawn)
+			{
+				let len = this.m_colBaseLen[col] + this.m_tableOpt.column_fill_length * 2;
+				str += this._fillStr('-', len);
+				str += '+';
+			}
+			else if (!first_line && this.m_metaRows.length > (row + 1))
+			{
+				// extend divide line if next row has extra columns
+				let columns_next = this.m_metaRows[row + 1].colFields;
+				if (columns_next.length > columns.length && col < columns_next.length)
+				{
+					let len = this.m_colBaseLen[col] + this.m_tableOpt.column_fill_length * 2;
+					str += this._fillStr('-', len);
+					str += '+';
+				}
+			}
+		}
+		return str;
+	}
+
+	_drawRow(row)
+	{
+		let str = '|';
+		let columns = this.m_metaRows[row].colFields;
+		let align = this.m_metaRows[row].colAlign;
+
+		for (let col = 0; col < this.m_colBaseLen.length; col++)
+		{
+			if (col < columns.length)
+			{
+				switch (align)
+				{
+					case 'left':
+					{
+						let len = (this.m_colBaseLen[col] - columns[col].colTextLen) + this.m_tableOpt.column_fill_length * 2;
+						str += (len < 2 ? '' : ' ') + columns[col].colText;
+						str += this._fillStr(' ', len < 2 ? len : len - 1);
+						break;
+					}
+					case 'right':
+					{
+						let len = (this.m_colBaseLen[col] - columns[col].colTextLen) + this.m_tableOpt.column_fill_length * 2;
+						str += this._fillStr(' ', len < 2 ? len : len - 1);
+						str += columns[col].colText + (len < 2 ? '' : ' ');
+						break;
+					}
+					case 'center':
+					{
+						let len = (this.m_colBaseLen[col] - columns[col].colTextLen) / 2;
+						str += this._fillStr(' ', Math.floor(len) + this.m_tableOpt.column_fill_length);
+						str += columns[col].colText;
+						str += this._fillStr(' ', Math.ceil(len) + this.m_tableOpt.column_fill_length);
+						break;
+					}
+				}
+				str += '|';
+			}
+			else if (this.m_tableOpt.column_empty_drawn)
+			{
+				let len = this.m_colBaseLen[col] + this.m_tableOpt.column_fill_length * 2;
+				str += this._fillStr(' ', len);
+				str += '|';
+			}
+		}
+		return str;
+	}
+
 	/*
-	# Get printable string lines in the terminal.
+	# Get printable string lines.
 	*/
 	lines()
 	{
@@ -129,51 +183,74 @@ class CharTable
 	}
 
 	/*
-	# Get printable string in the terminal.
+	# Get printable string.
 	*/
 	string(left_indentation = 0, first_line_break = false)
 	{
-		return (first_line_break ? '\n' : '') + this._fillStr(' ', left_indentation) + this.lines().join('\n' + this._fillStr(' ', left_indentation));
+		let a = first_line_break ? '\n' : '';
+		let b = this._fillStr(' ', left_indentation);
+		let c = this.lines().join('\n' + this._fillStr(' ', left_indentation));
+		return a + b + c;
 	}
 
 	/*
 	# Add a new row at the top of the table.
-	# Example: CharTable.insert(['xxx', 'xxx', ...]);
+	# The optional param 'align' can set the columns alignment of this row independently.
+	# Example: CharTable.insert(['xxx', 'xxx', ...], align);
 	*/
-	insert(columns)
+	insert(columns, align = undefined)
 	{
-		if (columns.length == 0) { return false; }
-		this.m_rowData.unshift(this._buildRowColumns(columns));
-		this.m_markUpdated = true;
-		return true;
+		if (columns.length > 0)
+		{
+			this.m_metaRows.unshift(this._buildRowData(columns, align));
+			this.m_markUpdated = true;
+		}
+		return this;
 	}
 
 	/*
 	# Add a new row at the bottom of the table.
-	# Example: CharTable.append(['xxx', 'xxx', ...]);
+	# The optional param 'align' can set the columns alignment of this row independently.
+	# Example: CharTable.append(['xxx', 'xxx', ...], align);
 	*/
-	append(columns)
+	append(columns, align = undefined)
 	{
-		if (columns.length == 0) { return false; }
-		this.m_rowData.push(this._buildRowColumns(columns));
-		this.m_markUpdated = true;
-		return true;
+		if (columns.length > 0)
+		{
+			this.m_metaRows.push(this._buildRowData(columns, align));
+			this.m_markUpdated = true;
+		}
+		return this;
 	}
 
 	/*
-	# Set table rows from given data.
-	# Example: CharTable.from([['xxx', 'xxx', ...], ['xxx', ...]]);
+	# Set the table rows from given data.
+	# The optional param 'align' can set the columns alignment of all rows.
+	# Example: CharTable.from([['xxx', 'xxx', ...], ['xxx', ...]], align);
 	*/
-	from(rows)
+	from(rows, align = undefined)
 	{
-		if (rows.length == 0) { return false; }
-		for (let columns of rows)
+		if (rows.length > 0)
 		{
-			if (columns.length > 0)
-			{ this.m_rowData.push(this._buildRowColumns(columns)); }
+			for (let columns of rows) { this.append(columns, align); }
+			this.m_markUpdated = true;
 		}
-		this.m_markUpdated = true;
-		return true;
+		return this;
+	}
+
+	/*
+	# Set the table rows from given data.
+	# The optional prop 'align' in each row object can set the columns alignment of that row.
+	# Example: CharTable.from2([{columns: ['xxx', ...], align: 'left'}, {columns: ['xxx', ...], align: 'right'}]);
+	*/
+	from2(row_objects)
+	{
+		if (row_objects.length > 0)
+		{
+			for (let row of row_objects) { this.append(row.columns, row.align); }
+			this.m_markUpdated = true;
+		}
+		return this;
 	}
 
 	/*
@@ -181,8 +258,8 @@ class CharTable
 	*/
 	clear()
 	{
-		this.m_rowData = [];
-		this.m_colMaxLen = [];
+		this.m_metaRows = [];
+		this.m_colBaseLen = [];
 		this.m_cachedTableLines = [];
 		this.m_markUpdated = false;
 		return this;
